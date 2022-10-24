5189da7e40d3c96131e9f98ca5af6ebd114d8a5a
indi rahayu ningsi <indiningsi9@gmail.com>
2022-10-24 11:07:36 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "hello world<br>";
+
+
+$nama = "Indi Rahayu Ningsi";
+$umur = "20";
+
+echo "nama saya <strong>$nama</strong>, saya berusia $umur tahun <br>";
+
+$namaadik = "Greshia";
+$umuradik = "13";
+
+echo "nama adik saya <strong>$namaadik</strong>, adik saya berusia $umuradik tahun <br>";
+
+$selisihumur = $umur - $umuradik;
+
+echo "selisih usia saya dan adik saya adalah $selisihumur tahun"
+
+?>
